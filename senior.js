59b8166742f57101049fbe10113762dd59c448b4
iraklimargvelashvili <ecseminari@gmail.com>
2025-03-23 16:07:02 +0400

    forth_change

--- senior.js
+++ senior.js
@@ -5,3 +5,8 @@ console.log("We are the craziest programmers!!!")
 console.log("It's raining today")
 
 console.log("It's cold")
+
+function change(){
+    console.log("it's cold")
+    return 100
+}
